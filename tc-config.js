991fd9eda0a34e5fa1ae0275471bb389a2ae82f1
CBell5-1010 <cwitche@uwo.ca>
2025-05-21 15:24:21 -0400

    Initial commit

---
+++ tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://_oxYJ82ZivoMAy1j-HUpese0D9fnV4Mq_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Understanding GenAI to support student researchers in an undergrad lab courses"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
